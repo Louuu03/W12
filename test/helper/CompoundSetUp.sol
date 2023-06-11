0ece6c9d637ffed124786301610026aa240f339e
Lou <lou.yiw@gmail.com>
2023-06-12 00:14:11 +0800

    W13

--- test/helper/CompoundSetUp.sol
+++ test/helper/CompoundSetUp.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "forge-std/console.sol";
 import "./UToken";
 import "lib/compound-protocol/contracts/WhitePaperInterestRateModel.sol";
 import "lib/compound-protocol/contracts/CErc20Delegate.sol";
